1c9df305e9dd93feea7c0ff2b726f070cef93bbf
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2024-02-08 22:00:49 -0300

    turns out changing the state the user needs every minute is not 'really cool', who would've thought

--- src/stores/clockStore.js
+++ src/stores/clockStore.js
@@ -13,27 +13,14 @@ const newTimezoneListArray = timezoneListArray.map(item => ({
   countryFlag: getFlagEmoji(item.countryCode), // Assuming countryCode is part of your data
 }));
 
-const useClockStore = create((set) => {
-  const updateClock = () => {
-    set(state => ({
-      currentTime: DateTime.now(),
-    }));
-  };
-
-  // Update the clock every second
-  const startClock = () => {
-    updateClock(); // Update immediately
-    setInterval(updateClock, 1000);
-  };
-
-  return {
+const useClockStore = create((set) => ({
     timezoneList: newTimezoneListArray,
     currentTime: DateTime.now(),
     selectedTime: DateTime.now(),
     currentTimezone: DateTime.now().zoneName,
     setSelectedTime: (selectedTime) => set({ selectedTime }),
-    startClock,
-  };
-});
+    // selectedTimezone: "",
+    // setSelectedTimezone: (selectedTimezone) => set({ selectedTimezone }),
+  }));
 
   export default useClockStore;
