b84b9be36d4d2e7ad91283a3ba9ff2a55fe468e1
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2024-02-09 00:20:33 -0300

    filtered redundant times (like the 20 USA times that only SOMETIMES change offset)

--- src/stores/clockStore.js
+++ src/stores/clockStore.js
@@ -8,13 +8,27 @@ let timezoneListArray = getTimeZones()
 // Sort the array by alternativeName
 timezoneListArray.sort((a, b) => a.countryName.localeCompare(b.countryName));
 // Add the countryFlag property to each object
-const newTimezoneListArray = timezoneListArray.map(item => ({
+const countryFlagTimezoneList = timezoneListArray.map(item => ({
   ...item,
   countryFlag: getFlagEmoji(item.countryCode), // Assuming countryCode is part of your data
 }));
 
+// Filter out timezones with duplicate offsets and country names
+const filteredTimezoneList = countryFlagTimezoneList.filter((timezone, index, self) => {
+  const hasDuplicate = self.some((otherTimezone, otherIndex) => {
+    return (
+      index !== otherIndex &&
+      timezone.currentTimeOffsetInMinutes === otherTimezone.currentTimeOffsetInMinutes &&
+      timezone.countryName === otherTimezone.countryName
+    );
+  });
+
+  return !hasDuplicate;
+});
+
+
 const useClockStore = create((set) => ({
-    timezoneList: newTimezoneListArray,
+    timezoneList: filteredTimezoneList,
     currentTime: DateTime.now(),
     selectedTime: DateTime.now(),
     currentTimezone: DateTime.now().zoneName,
